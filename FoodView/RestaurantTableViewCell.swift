22c2e448c37c46e891e6307eed459da3a04e97f4
Anthony Schwartz <anthonyschwartz@anthonys-imac.local>
2018-06-13 14:15:45 -0500

    Initial Commit

---
+++ FoodView/RestaurantTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodView
+//
+//  Created by Anthony Schwartz on 6/13/18.
+//  Copyright © 2018 Anthony Schwartz. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
